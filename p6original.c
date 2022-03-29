d289502cbfc13b609f63a52e313ef86abc2872c3
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-03-29 12:57:39 +0000

    p1original.c

---
+++ p6original.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char *string)
+{
+  printf("Enter the string:\n");
+  scanf("%[^\n]",string);
+}
+int count_words1(char *string)
+{
+  int i=0;
+  char* word;
+  word=strtok(string, " ,\".-");
+  while(word != NULL)
+    {
+      word=strtok(NULL, " ,\".-");
+      i++;
+    }
+  return i;
+}
+void output(char *string,int no_words)
+{
+  printf("The number of words is %d\n",no_words);
+}
+int main()
+{
+  char string[50];
+  input_string(string);
+  int words=count_words1(string);
+  output(string,words);
+  return 0;
+}
